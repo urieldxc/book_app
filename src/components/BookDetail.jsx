737aab3bdee5180953771834613f0e25a800c4fb
Sergio Tomás <sergiota.cs@gmail.com>
2023-03-02 15:50:54 +0100

    Details working, need to style

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-const BookDetail = () => {
+const BookDetail = ({ updatedList }) => {
     const { isbn } = useParams()
+    const [bookDetails, setBookDetails] = useState()
 
+    const filterBookDetail = (isbn) =>{
+        const filtered = updatedList.find(book => book.isbn === isbn)
+        setBookDetails(filtered)
+    }
+
+    useEffect(() => {
+        if(updatedList) filterBookDetail(isbn)
+    }, [updatedList, isbn])
+    
+    
     return (
-        <div>{isbn}</div>
+        <div>
+            {bookDetails && (
+                <div>
+                    <div>{bookDetails.title}</div>
+                    <div>{bookDetails.isbn}</div> 
+                </div>
+            )}
+        </div>
     )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,13 +9,13 @@ import { useState } from 'react';
 const Main = () => {
 
   const [updatedList, setUpdatedList] = useState([])
-  console.log(updatedList)
+
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/books' element={<BookList updatedList={updatedList} setUpdatedList={setUpdatedList}/>} />
-        <Route path='/books/:isbn' element={<BookDetail />} />
+        <Route path='/books/:isbn' element={<BookDetail updatedList={updatedList}/>} />
       </Routes>
     </div>
   )
